edb839a2dfc2eff1e69fad4e33de68e32470e523
r33c3 <reecem@ecu.edu>
2020-05-19 09:52:04 -0400

    cleanup; secrets files

--- AuthorityCouch/Repositories/ArchivesSpaceRepo.cs
+++ AuthorityCouch/Repositories/ArchivesSpaceRepo.cs
@@ -6,7 +6,6 @@ using AuthorityCouch.Helpers;
 using AuthorityCouch.Models;
 using AuthorityCouch.Models.Import;
 using NPoco;
-using NPoco.Expressions;
 
 namespace AuthorityCouch.Repositories
 {
@@ -58,9 +57,6 @@ namespace AuthorityCouch.Repositories
                 .Select("subject.id, subject.title AS subject, (select value from archivesspace.enumeration_value where id = (select term_type_id from archivesspace.term WHERE id = (select term_id from archivesspace.subject_term where subject_id = subject.id))) AS type")
                 .From("subject_rlshp")
                 .LeftJoin("subject").On("subject.id = subject_rlshp.subject_id")
-                //.LeftJoin("subject_term").On("subject_term.subject_id = subject_rlshp.subject_id")
-                //.LeftJoin("term").On("term.id = subject_term.id")
-                //.LeftJoin("enumeration_value").On("enumeration_value.id = term.term_type_id")
                 .Where("resource_id = @0", resourceId);
 
             using (var db = Connection)
@@ -75,19 +71,6 @@ namespace AuthorityCouch.Repositories
         public List<AgentGroup> GetLinkedAgents()
         {
             var retVal = new List<AgentGroup>();
-            //var agentSql = Sql.Builder
-            //    .Select("linked_agents_rlshp.agent_person_id, linked_agents_rlshp.agent_family_id, linked_agents_rlshp.agent_corporate_entity_id, " +
-            //            "linked_agents_rlshp.role_id, linked_agents_rlshp.resource_id, name_person.sort_name AS person_name, " +
-            //            "name_family.sort_name AS family_name, name_corporate_entity.sort_name AS corp_name")
-            //    .From("linked_agents_rlshp")
-            //    .LeftJoin("agent_person").On("agent_person.id = linked_agents_rlshp.agent_person_id")
-            //    .LeftJoin("name_person").On("name_person.agent_person_id = linked_agents_rlshp.agent_person_id")
-            //    .LeftJoin("agent_family").On("agent_family.id = linked_agents_rlshp.agent_family_id")
-            //    .LeftJoin("name_family").On("name_family.agent_family_id = linked_agents_rlshp.agent_family_id")
-            //    .LeftJoin("agent_corporate_entity").On("agent_corporate_entity.id = linked_agents_rlshp.agent_corporate_entity_id")
-            //    .LeftJoin("name_corporate_entity").On("name_corporate_entity.agent_corporate_entity_id = linked_agents_rlshp.agent_corporate_entity_id")
-            //    .Where("resource_id IS NOT NULL")
-            //    .OrderBy("person_name, family_name, corp_name");
 
             var personSql = Sql.Builder.Select("linked_agents_rlshp.agent_person_id, linked_agents_rlshp.role_id, linked_agents_rlshp.resource_id, name_person.sort_name AS person_name, authority_id, resource.repo_id")
                 .From("archivesspace.linked_agents_rlshp")
@@ -129,9 +112,6 @@ namespace AuthorityCouch.Repositories
                 .From("subject_rlshp")
                 .LeftJoin("subject").On("subject.id = subject_rlshp.subject_id")
                 .LeftJoin("archivesspace.resource").On("resource.id = subject_rlshp.resource_id")
-                //.LeftJoin("subject_term").On("subject_term.subject_id = subject_rlshp.subject_id")
-                //.LeftJoin("term").On("term.id = subject_term.id")
-                //.LeftJoin("enumeration_value").On("enumeration_value.id = term.term_type_id")
                 .Where("resource_id IS NOT NULL");
 
             using (var db = Connection)
